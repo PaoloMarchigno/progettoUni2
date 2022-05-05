8a4ec81d48f4a4065508cc432d7265e09de7d6c4
thoms2901 <thomas.kirschner2901@gmail.com>
2022-05-05 17:59:41 +0200

    Cambio da php a node

---
+++ utente.js
@@ -0,0 +1,47 @@
+const { Client } = require('pg');
+
+function inserisciUtente(client, username, email, password) {
+    const query = {
+	text: 'INSERT INTO utente(username, email, password) VALUES($1, $2, $3)',
+	values: [username, email, password]
+    };
+    client.query(query, (err, res) => {
+	if (err) {
+	    console.log(err.stack);
+	} else {
+	    console.log(res.rows);
+	}
+    });
+}
+
+async function controlloSeEsisteUtente(client, email) {
+    try {
+	    const query = {
+	        text: 'SELECT * FROM utente WHERE email = $1',
+	        values: [email]
+	    };
+	    var result = await client.query(query);
+		return result.rows.length > 0;
+    } catch (err) {
+	    console.log(err.stack);
+		process.exit(1);
+    }
+}
+
+async function getPasshash(client, email) {
+    try {
+	    const query = {
+	        text: 'SELECT password FROM utente WHERE email = $1',
+	        values: [email]
+	    };
+	    var result = await client.query(query);
+    } catch (err) {
+	    console.log(err.stack);
+		process.exit(1);
+    }
+    return result.rows[0].password;
+}
+
+exports.inserisciUtente = inserisciUtente;
+exports.controlloSeEsisteUtente = controlloSeEsisteUtente;
+exports.getPasshash = getPasshash;
