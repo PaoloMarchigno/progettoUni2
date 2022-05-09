797c2501206a0e8c24c22ea88b936fa546b29d10
thoms2901 <thomas.kirschner2901@gmail.com>
2022-05-09 22:50:20 +0200

    db-2

--- db.js
+++ db.js
@@ -9,14 +9,14 @@ const db = new Client({
   port: 5555,
 }); 
 
-const chall = new Client({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'challenges',
-  password: 'root',
-  port: 5433,
-});
+// const chall = new Client({
+//   user: 'postgres',
+//   host: '127.0.0.1',
+//   database: 'challenges',
+//   password: 'root',
+//   port: 5433,
+// });
 
-exports.chall = chall;
+// exports.chall = chall;
 
 exports.db = db;
--- funzioni_db.js
+++ funzioni_db.js
@@ -16,27 +16,48 @@ db.connect( (err) => {
 	}
 });
 
+
+function eseguiQuery(db, query){
+    db.query(query, (err, res) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            console.log(res.rows);
+        }
+        });
+}
+
 function inserisciChallenge(db, c) {
     const query = {
 	text: 'INSERT INTO challenge(id, nome, testo, score, n_utenti_solv, flag) VALUES($1, $2, $3, $4, $5, $6)',
 	values: [c.id, c.nome, c.testo, c.score, c.n_utenti_solv, c.flag]
     };
-    db.query(query, (err, res) => {
-	if (err) {
-	    console.log(err.stack);
-	} else {
-	    console.log(res.rows);
-	}
-    });
+    eseguiQuery(db, query);
+}
+
+function modificaChallenge(db, c){
+    eliminaChallenge(db, c.id);
+    inserisciChallenge(db, c);
+}
+
+function eliminaChallenge(db, id){
+    const query = {
+        text: 'DELETE FROM challenge where id=$1',
+        values: [id]
+        };
+    eseguiQuery(db, query);
+
 }
 
 var challenge ={
     id : 1,
     nome : 'Infinite Server',
     testo : 'Questo server sembra avere molte pagine, sono sicuro che qualcuno arriverà alla fine... prima o poi. Indirizzo: http://infinite.challs.olicyber.it',
-    score : 100,
+    score : 200,
     n_utenti_solv : 0,
     flag : 'flag{y0u_mu57_b3_4_r35lly_f457_cl1ck3r!}',
 }
 
-inserisciChallenge(db, challenge);
+// inserisciChallenge(db, challenge);
+// eliminaChallenge(db, 1);
+// modificaChallenge(db, challenge);
